6e22a73edf997b49e14e5924cf75cdd86382b6a1
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2015-03-12 10:20:31 -0400

    Projet libgdx de base

---
+++ android/src/com/alexfr/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.alexfr.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.alexfr.game.TowerClimber;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TowerClimber(), config);
+	}
+}
---
+++ desktop/src/com/alexfr/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alexfr.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alexfr.game.TowerClimber;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TowerClimber(), config);
+	}
+}
