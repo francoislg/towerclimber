4ac529b3258ff6cbd00963acb5cc5328fe8d54ac
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2015-03-19 10:56:35 -0400

    Update de la camera

--- core/src/com/alexfr/game/Camera.java
+++ core/src/com/alexfr/game/Camera.java
@@ -1,5 +1,6 @@
 package com.alexfr.game;
 
+import com.alexfr.game.Characters.Character;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,6 +15,8 @@ public class Camera {
 	public Camera(int width, int height){
 		camera = new OrthographicCamera(width, height);
 		camera.setToOrtho(true, width, height);
+		camera.rotate(180);
+		camera.translate(-width/2, -height/2);
 		camera.update();
 	}
 	
@@ -23,5 +26,9 @@ public class Camera {
 	
 	public void setSpriteBatchProjection(SpriteBatch batch){
 		batch.setProjectionMatrix(camera.combined);
+	}
+	
+	public void follow(Character character){
+		camera.translate(character.getPosition());
 	}
 }
