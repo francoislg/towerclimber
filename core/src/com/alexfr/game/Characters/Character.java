442a6ff16fdf853a4fd501061716a36593a9ed4f
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2015-03-19 15:45:44 -0400

    Début d'une collision pour sauter uniquement au sol

--- core/src/com/alexfr/game/Characters/Character.java
+++ core/src/com/alexfr/game/Characters/Character.java
@@ -6,15 +6,21 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Character implements Controllable, Renderable {
 	
-	private Vector2 size = new Vector2(50, 50);
-	private Vector2 speed = new Vector2(1, 5);
+	private final Vector2 size = new Vector2(250, 250);
+	private final Vector2 boxSize = new Vector2(size.x/2, size.y/2);
+	private final Vector2 speed = new Vector2(1, 5);
+	private final float jumpForce = 10;
 	Body body;
+	
+	Fixture bodyFixture;
+	Fixture feetFixture;
 	
 	public Character(World world) {
 		BodyDef bodyDef = new BodyDef();
@@ -23,13 +29,22 @@ public class Character implements Controllable, Renderable {
         body = world.createBody(bodyDef);
 		
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(size.x/2, size.y/2);
+        shape.setAsBox(boxSize.x, boxSize.y);
         
 		FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
 
-        body.createFixture(fixtureDef);
+        bodyFixture = body.createFixture(fixtureDef);
+        
+        PolygonShape canIJumpShape = new PolygonShape();
+        canIJumpShape.setAsBox(boxSize.x, 1, new Vector2(0, boxSize.y), 0);
+        
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = canIJumpShape;
+        fixtureDef.isSensor = true;
+        
+        feetFixture = body.createFixture(fixtureDef);
         
         shape.dispose();
 	}
@@ -51,8 +66,10 @@ public class Character implements Controllable, Renderable {
 
 	@Override
 	public void jump(){
-		float impulse = body.getMass() * 10;
-		body.applyLinearImpulse(new Vector2(0, -impulse), body.getWorldCenter(), true);
+		float impulse = body.getMass() * jumpForce;
+		if(feetFixture.testPoint(5, 1)){
+			body.applyLinearImpulse(new Vector2(0, -impulse), body.getWorldCenter(), true);
+		}
 	}
 	
 	@Override
