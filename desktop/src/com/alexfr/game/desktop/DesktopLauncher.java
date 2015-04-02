c862460ced41b4155bd84469f4617b6a68bf4104
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2015-04-02 10:43:49 -0400

    Changé la taille des fenêtres

--- desktop/src/com/alexfr/game/desktop/DesktopLauncher.java
+++ desktop/src/com/alexfr/game/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package com.alexfr.game.desktop;
 
+import com.alexfr.game.TowerClimber;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.alexfr.game.TowerClimber;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TowerClimber(), config);
-	}
+    public static void main(String[] arg) {
+	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	config.foregroundFPS = 60;
+	config.backgroundFPS = 30;
+	config.width = 1280;
+	config.height = 720;
+	new LwjglApplication(new TowerClimber(), config);
+    }
 }
--- html/src/com/alexfr/game/client/HtmlLauncher.java
+++ html/src/com/alexfr/game/client/HtmlLauncher.java
@@ -1,19 +1,21 @@
 package com.alexfr.game.client;
 
+import com.alexfr.game.TowerClimber;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.alexfr.game.TowerClimber;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+	GwtApplicationConfiguration config = new GwtApplicationConfiguration(1280, 720);
+	config.antialiasing = true;
+	return config;
+    }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new TowerClimber();
-        }
+    @Override
+    public ApplicationListener getApplicationListener() {
+	return new TowerClimber();
+    }
 }
